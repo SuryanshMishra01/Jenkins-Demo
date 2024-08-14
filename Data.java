ce3a42f80aab9414e580cb2a595715d2dae3bb84
Suryansh Mishra <116947777+SuryanshMishra01@users.noreply.github.com>
2024-08-14 10:51:21 +0530

    Update Data.java

--- Data.java
+++ Data.java
@@ -4,11 +4,10 @@ class Data{
   public static void main(String args[]){
 	
     Scanner sc = new Scanner(System.in);
-    System.out.println("Enter two numbers: ");
-    int a = sc.nextInt();
-    int b = sc.nextInt();
+    System.out.println("the two numbers: 10 & 20");
+    
     
     System.out.println("The sum of the numbers is: ");
-    System.out.println(a+b);
+    System.out.println(10+20);
    }
 }
