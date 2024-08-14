124fe1329be2f5b5dc7ae75389ff50d1cd6d9e47
Suryansh Mishra <116947777+SuryanshMishra01@users.noreply.github.com>
2024-08-14 10:29:21 +0530

    Add files via upload

--- Data.java
+++ Data.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 class Data{
   public static void main(String args[]){
-	System.out.println("Hello World");
+	
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter two numbers: ");
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    
+    System.out.println("The sum of the numbers is: ");
+    System.out.println(a+b);
    }
 }
